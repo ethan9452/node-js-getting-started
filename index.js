978efe14f0e9e33f261922ea3c983700849e51be
ethan9452 <ethan9452@gmail.com>
2019-04-20 02:36:54 -0700

    added some stuff

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+// express() is the constructor of an 'express' object i think
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'public'))) // __dirname is full path of project home directory
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .get('/test', (req, res) => res.send('This is a test page'))
+  .get('/test-log', (req, res) => {console.log('test test123'); res.send('Test line logged')})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
